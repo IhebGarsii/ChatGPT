5bb02c3a99b607353527dd64e0c2b1acb46be92c
IhebGarsii <ihebgarsi78@gmail.com>
2024-02-16 23:38:10 +0100

    home page

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="left">
+        <h1>Lets Build Something amazing with GPT-3 OpenAI</h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <div className="input-container">
+          <input type="text" placeholder="Your Email Address" />
+          <button>Get Started</button>
+        </div>
+      </div>
+      <div className="right"></div>
+    </div>
+  );
+}
+
+export default Home;
