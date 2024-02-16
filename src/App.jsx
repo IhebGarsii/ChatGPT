5dab79a624b5315ad1e15d39061da1d8cd0c6f5a
IhebGarsii <ihebgarsi78@gmail.com>
2024-02-16 13:47:51 +0100

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Navbar from "./componant/navbar/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      
-    </>
+    <div className="App">
+      <Navbar />
+    </div>
   );
 }
 
---
+++ src/componant/navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./navbar.css";
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="left">
+        <div className="logo">
+          <h1>GPT3</h1>
+        </div>
+        <div className="nav">
+          <a href="">Home</a>
+          <a href="">What is GPT3</a>
+          <a href="">Open AI</a>
+          <a href="">Case Studies</a>
+          <a href="">Library</a>
+          <a href="">Home</a>
+        </div>
+      </div>
+      <div className="right">
+        <button>Sign in</button>
+        <button>Sign up</button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
